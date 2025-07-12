cc01f5245207ffefc5c4e7862231059182511f67
srisha047 <srisha047@gmail.com>
2025-07-12 03:26:14 +0000

    Deploying to gh-pages from @ srisha047/srisha047.github.io@b90825fc4453c331888b4931ac7f69e221fb9f02 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,8 +430,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-started-job-as-s-senior-project-manager-at-saaho-nepal-sparkles-smile",
+          title: 'Started Job as s Senior Project Manager at SAAHO NEPAL!!! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
