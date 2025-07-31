e1d9f8b90062184b03e8b7e2dfd8276f9208a5a9
srisha047 <srisha047@gmail.com>
2025-07-31 05:26:08 +0000

    Deploying to gh-pages from @ srisha047/srisha047.github.io@8b9b3f6c51d840ada35dc5f9d80090aaa26d9985 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "Projects",
           description: "List of projects I&#39;ve worked on.",
           section: "Navigation",
